b81772e45333f0da3ba032cb77ee43553d93cb84
Eliel Martins <elieuprata@gmail.com>
2024-01-12 19:15:36 -0300

    finalize project

--- assets/js/pokemon-page.js
+++ assets/js/pokemon-page.js
@@ -5,30 +5,58 @@ let pokemonSelected = {}
 const urlParams = new URLSearchParams(window.location.search)
 const id = urlParams.get('id')
 
+function showCategory(categoryName) {
+  document.getElementById('divStats')
+}
+
 pokeApi.getPokemon(parseInt(id)).then((pokemon) => {
   console.log(pokemon)
 
   const abilities = pokemon.abilities.map((item) => item.ability.name)
 
+  const stats = pokemon.stats
+    .map((item) => {
+      const name =
+        item.stat.name.search('special-') !== -1
+          ? item.stat.name.replace('special-', 'sp. ').substring(0, 7) + '.'
+          : item.stat.name
+
+      return `
+      <div class="infoItem">
+        <span class="infoDescription">${name}</span>
+        <span class="infoValue">${item.base_stat}</span>
+        <progress class="${
+          item.base_stat > 50 ? 'statProgress greaterThan50' : 'statProgress'
+        }" value="${item.base_stat}" max="100"></progress>
+      </div>`
+    })
+    .join('')
+
   info.innerHTML = `
-    <div class="infoItem">
-      <span class="infoDescription">Species</span>
-      <span class="infoValue">${pokemon.species.name}</span>
+    <div id="divAbout">
+      <div class="infoItem">
+        <span class="infoDescription">Species</span>
+        <span class="infoValue">${pokemon.species.name}</span>
+      </div>
+
+      <div class="infoItem">
+        <span class="infoDescription">Height</span>
+        <span class="infoValue">${pokemon.height * 10} cm</span>
+      </div>
+
+      <div class="infoItem">
+        <span class="infoDescription">Weigh</span>
+        <span class="infoValue">${pokemon.weight * 0.1} kg</span>
+      </div>
+
+      <div class="infoItem">
+        <span class="infoDescription">Abilities</span>
+        <span class="infoValue">${abilities.join(', ')}</span>
+      </div>
     </div>
 
-    <div class="infoItem">
-      <span class="infoDescription">Height</span>
-      <span class="infoValue">${pokemon.height * 10} cm</span>
-    </div>
-
-    <div class="infoItem">
-      <span class="infoDescription">Weigh</span>
-      <span class="infoValue">${pokemon.weight * 0.1} kg</span>
-    </div>
-
-    <div class="infoItem">
-      <span class="infoDescription">Abilities</span>
-      <span class="infoValue">${abilities.join(', ')}</span>
+    <div id="divStats" style="display: none;">
+      ${stats}
     </div>
   `
 
@@ -46,3 +74,19 @@ pokeApi.getPokemon(parseInt(id)).then((pokemon) => {
 
   pokemonSelected = pokemon
 })
+
+function showCategory(categoryName) {
+  if (categoryName === 'about') {
+    document.getElementById('divAbout').style.display = 'block'
+    document.getElementById('divStats').style.display = 'none'
+
+    document.getElementById('btnAbout').classList.add('selected')
+    document.getElementById('btnStats').classList.remove('selected')
+  } else if (categoryName === 'stats') {
+    document.getElementById('divAbout').style.display = 'none'
+    document.getElementById('divStats').style.display = 'block'
+
+    document.getElementById('btnAbout').classList.remove('selected')
+    document.getElementById('btnStats').classList.add('selected')
+  }
+}
